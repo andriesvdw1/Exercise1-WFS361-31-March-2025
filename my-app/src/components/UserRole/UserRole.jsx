eb763f06b67fc1d664efb2522576a42c20a22e20
andriesvdw1 <andriesvdw1@gmail.com>
2025-04-01 12:00:13 +0200

    added a switch case statement to display a message on the screen depending on the user's role.

--- my-app/src/components/UserRole/UserRole.jsx
+++ my-app/src/components/UserRole/UserRole.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './UserRole.css'
-function UserRole() {
-  return (
-    <div>UserRole</div>
-  )
+
+function UserRole({role}) {
+    const roleDescriptions = {
+        'admin': "You have full access.",
+        'editor': "You can edit content.",
+        'viewer': "You can view content only.",
+    };
+
+    let message = "";
+
+    switch (role) {
+        case 'admin':
+            message = roleDescriptions['admin'];
+            break;
+        case 'editor':
+            message = roleDescriptions['editor'];
+            break;
+        case 'viewer':
+            message = roleDescriptions['viewer'];
+            break;
+        default:
+            message = "Unknown role";
+            break;
+    }
+
+    return (
+        <div>{message}</div>
+    )
 }
 
 export default UserRole
